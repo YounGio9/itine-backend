42a5bad8e1e6fca1c098742521fe7c90a41c6411
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-05 19:12:55 +0100

    feat: Update User model

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "firstName" VARCHAR(255) NOT NULL,
+    "lastName" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" TEXT NOT NULL,
+    "country" VARCHAR(255) NOT NULL,
+    "town" VARCHAR(255) NOT NULL,
+    "phoneNumber" INTEGER NOT NULL,
+    "isAdmin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
