6853f3d95ee2d1352bde06ea6d154d0d3e849501
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-05 20:00:16 +0100

    feat: Remove isAdmin column from User model

--- prisma/migrations/20230905181615_init/migration.sql
+++ prisma/migrations/20230905181615_init/migration.sql
@@ -1,3 +1,6 @@
+-- CreateEnum
+CREATE TYPE "RoleEnumType" AS ENUM ('user', 'admin');
+
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
@@ -8,6 +11,7 @@ CREATE TABLE "User" (
     "country" VARCHAR(255) NOT NULL,
     "town" VARCHAR(255) NOT NULL,
     "phoneNumber" INTEGER NOT NULL,
+    "role" "RoleEnumType" DEFAULT 'user',
     "isAdmin" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
@@ -15,4 +19,3 @@ CREATE TABLE "User" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
