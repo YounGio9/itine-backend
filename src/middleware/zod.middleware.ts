dc02da10a1c0982d6ff75b1ea2792dfd648d37e0
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-09 20:19:28 +0100

    feat: Change zod errors format in `zodValidator`

--- src/middleware/zod.middleware.ts
+++ src/middleware/zod.middleware.ts
@@ -14,10 +14,16 @@ const zodValidator =
             next()
             return
         }
+
+        const errors: any = {}
+
+        result.error.issues.forEach((issue) => {
+            errors[issue.path[1]] = issue.message
+        })
         // eslint-disable-next-line consistent-return
         return res.status(500).json({
             success: result.success,
-            message: result.error,
+            errors,
         })
     }
 
