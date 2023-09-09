83930f04e2aeeaf97db5ea39016d04ced9d8e4b8
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-09 20:41:07 +0100

    fix: Fix bad regex for createUser password

--- src/resources/user/user.validation.ts
+++ src/resources/user/user.validation.ts
@@ -9,7 +9,7 @@ const createUser = z.object({
             .string()
             .min(8, 'Password should have at least 8 characters.')
             .regex(
-                /'^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]/,
+                /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]/,
                 'Password should have alphanumeric characters and at least one special character.',
             ),
         country: z.string(),
