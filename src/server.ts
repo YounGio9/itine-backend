1272f82e18b1a3c98d51a119693e75ad63146aa8
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-09 20:42:20 +0100

    feat: Add AuthController to main app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'module-alias/register'
 import 'dotenv/config'
 import validateEnv from '@utils/validateEnv'
+import UserController from '@resources/user/user.controller'
+import AuthController from '@resources/auth/auth.controller'
 import App from './app'
 
 validateEnv()
 
-const app = new App([], Number(process.env.PORT))
+const app = new App([new UserController(), new AuthController()], Number(process.env.PORT))
 app.listen()
