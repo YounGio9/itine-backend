0c9134df29d1dd551b91130a5e8114dab6f283c0
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-10 08:49:51 +0100

    feat: Update userInterface

--- src/resources/user/user.interface.ts
+++ src/resources/user/user.interface.ts
@@ -4,6 +4,7 @@ import type { createUser } from './user.validation'
 type User = z.infer<typeof createUser.shape.body> & {
     id: number
     role: 'admin' | 'user'
+    refreshToken: string | null
 }
 
 export default User
