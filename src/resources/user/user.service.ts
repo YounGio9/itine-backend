e9c98e8e523c1636f8e0ea4fa3676befdab52949
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-10 08:58:21 +0100

    feat: Add new methods to handle users in `UserService`

--- src/resources/user/user.service.ts
+++ src/resources/user/user.service.ts
@@ -31,6 +31,40 @@ class UserService {
             throw new Error('Unable to find Users')
         }
     }
+
+    public async getByRefreshToken(refreshToken: string): Promise<User | null> {
+        try {
+            const user = await this.user.findUnique({
+                where: {
+                    refreshToken,
+                },
+            })
+
+            return user
+        } catch (error) {
+            throw new Error('Cant find user')
+        }
+    }
+
+    public async getByEmail(email: string): Promise<User | null> {
+        try {
+            const user = await this.user.findUnique({ where: { email } })
+            return user
+        } catch (error) {
+            throw new Error('Cant find user')
+        }
+    }
+
+    public async updateByEmail(email: string, data: Partial<User>): Promise<void> {
+        try {
+            await this.user.update({
+                where: { email },
+                data: { ...data },
+            })
+        } catch (error) {
+            throw new Error('Cant create user')
+        }
+    }
 }
 
 export default UserService
