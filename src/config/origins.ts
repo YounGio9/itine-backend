429db24fd8aa7daeeb3d40973a6409b30406e39d
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-14 22:47:17 +0100

    feat: Add origin for prod frontend website

--- src/config/origins.ts
+++ src/config/origins.ts
@@ -1,3 +1,7 @@
-const appOrigins = ['http://localhost:3000', 'https://backend-dev-itine.onrender.com']
+const appOrigins = [
+    'http://localhost:3000',
+    'https://backend-dev-itine.onrender.com',
+    'http://itine.fr',
+]
 
 export default appOrigins
