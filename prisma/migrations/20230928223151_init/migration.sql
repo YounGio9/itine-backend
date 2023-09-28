0897ed7bad7ebee7bb5cfac41a5f248dae9c17e5
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-09-28 23:48:10 +0100

    feat: Add cascading delete to colorss and sizes of product

---
+++ prisma/migrations/20230928223151_init/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Color" DROP CONSTRAINT "Color_productId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Size" DROP CONSTRAINT "Size_productId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Size" ADD CONSTRAINT "Size_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Color" ADD CONSTRAINT "Color_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
