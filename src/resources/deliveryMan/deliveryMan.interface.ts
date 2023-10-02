206d939343610c5333e6de1bf59cf1dd8a781c4f
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-02 10:04:37 +0100

    feat: Update deliveryMan interface

--- src/resources/deliveryMan/deliveryMan.interface.ts
+++ src/resources/deliveryMan/deliveryMan.interface.ts
@@ -1,8 +1,9 @@
 import type { z } from 'zod'
 import type { createDeliveryMan } from './deliveryMan.validation'
 
-type DeliveryMan = z.infer<typeof createDeliveryMan.shape.body> & {
+type DeliveryMan = Omit<z.infer<typeof createDeliveryMan.shape.body>, 'dateOfBirth'> & {
     id: number
+    dateOfBirth: Date
 }
 
 export default DeliveryMan
