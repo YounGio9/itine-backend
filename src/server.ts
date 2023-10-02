2999fe51a843998ae280c996811c139dd0f39df3
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-02 11:03:22 +0100

    feat: Init deliveryMan controller in main app

--- src/server.ts
+++ src/server.ts
@@ -4,11 +4,12 @@ import validateEnv from '@utils/validateEnv'
 import UserController from '@resources/user/user.controller'
 import AuthController from '@resources/auth/auth.controller'
 import SizeController from '@resources/size/size.controller'
+import CityController from '@resources/city/city.controller'
 import ColorController from '@resources/color/color.controller'
 import MessageController from '@resources/message/message.controller'
 import ProductController from '@resources/product/product.controller'
 import CategoryController from '@resources/category/category.controller'
-import CityController from '@resources/city/city.controller'
+import DeliveryManController from '@resources/deliveryMan/deliveryMan.controller'
 import App from './app'
 
 validateEnv()
@@ -23,6 +24,7 @@ const app = new App(
         new ColorController(),
         new SizeController(),
         new CityController(),
+        new DeliveryManController(),
     ],
     Number(process.env.PORT),
 )
