8bea626a3f43d41cb9a267351f8c6075b359f824
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-04 07:05:59 +0100

    feat: Add status to deliveryMan interface

--- src/resources/deliveryMan/deliveryMan.interface.ts
+++ src/resources/deliveryMan/deliveryMan.interface.ts
@@ -4,6 +4,7 @@ import type { createDeliveryMan } from './deliveryMan.validation'
 type DeliveryMan = Omit<z.infer<typeof createDeliveryMan.shape.body>, 'dateOfBirth'> & {
     id: number
     dateOfBirth: Date
+    status: 'unset' | 'accepted' | 'rejected'
 }
 
 export default DeliveryMan
