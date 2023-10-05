321d7dc9b234e9ce569b3c510731ec6b25f5e1f7
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-05 01:47:29 +0100

    feat: Add schema validation for replyMessage payload

--- src/resources/message/message.validation.ts
+++ src/resources/message/message.validation.ts
@@ -9,8 +9,16 @@ const createMessage = z.object({
     }),
 })
 
+const replyMessage = z.object({
+    body: z.object({
+        email: z.string().email('Please enter a valid email address'),
+        subject: z.string(),
+        body: z.string(),
+    }),
+})
+
 const updateMessage = z.object({})
 
 type createMessageType = z.infer<typeof createMessage.shape.body>
 
-export { createMessage, updateMessage, type createMessageType }
+export { createMessage, replyMessage, updateMessage, type createMessageType }
