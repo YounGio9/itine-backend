95d15705b15454d56850f4503c02085370b6f21d
Giovani de SOUZA <giovanidesouza999@gmail.com>
2023-10-22 22:11:15 +0100

    feat: Remove special character in password for register validation

--- src/resources/auth/auth.validation.ts
+++ src/resources/auth/auth.validation.ts
@@ -7,7 +7,7 @@ const login = z.object({
             .string()
             .min(8, 'Password should have at least 8 characters.')
             .regex(
-                /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]/,
+                /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*()_+]/,
                 'Password should have alphanumeric characters and at least one special character.',
             ),
     }),
